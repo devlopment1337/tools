c9d7be8f1019679f4ac9c5080316a1a310459995
devlopment1337 <108673521+devlopment1337@users.noreply.github.com>
2022-08-28 18:51:42 +0600

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-echo "export GOPATH=$HOME/go" >> ~/.zshrc
-echo "export GOROOT=/usr/local/go" >> ~/.zshrc
-echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.zshrc
-
-wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-
-
-echo "export GOPATH=$HOME/go" >> ~/.zshrc
-echo "export GOROOT=/usr/local/go" >> ~/.zshrc
-echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.zshrc
-
-source ~/.zshrc
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/tomnomnom/waybackurls@latest
 go get -u github.com/tomnomnom/gf
