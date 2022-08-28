78ecab1cadc6499f4b3eb96f831387ac78cb9d14
devlopment1337 <108673521+devlopment1337@users.noreply.github.com>
2022-08-28 18:23:23 +0600

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+
+
+echo "export GOPATH=$HOME/go" >> ~/.zshrc
+echo "export GOROOT=/usr/local/go" >> ~/.zshrc
+echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> ~/.zshrc
+
+source ~/.zshrc
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/tomnomnom/waybackurls@latest
+go get -u github.com/tomnomnom/gf
+cd
+mkdir .gf
+cd
+git clone https://github.com/1ndianl33t/Gf-Patterns.git
+cd Gf-Patterns/
+mv *.json ~/.gf
+cd
+rm -rf Gf-Patterns
+apt install dirsearch -y
